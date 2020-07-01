25da361b073e18d02e69680563da434078d9db26
ljh <1373804509@qq.com>
2020-07-01 23:57:22 +0800

    changed at 7.1

---
+++ src/my_pac/User.java
@@ -0,0 +1,46 @@
+package my_pac;
+
+import javax.swing.*;
+
+public class User {
+    private String username;
+    private String password;
+
+    //Shift+Enter     不用到行尾换行
+    //Ctr+shift+A   命令行
+    //Alt+鼠标        列编辑
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static void main(String[] args) {
+        User a = new User("ljh","123456");
+        System.out.println(a.toString());
+    }
+}
